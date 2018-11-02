dfc310fdd81809014edd1b21bae852d34864bfa2
Jeremy Campbell <jeremyc61298@gmail.com>
2018-11-01 21:35:15 -0500

    Init input file handle

--- src/pancakes.java
+++ src/pancakes.java
@@ -1,2 +1,19 @@
+// --------------------------------------
+// pancakes.java
+// Jeremy Campbell
+// Practice using a Breadth First Search
+// --------------------------------------
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class pancakes {
+    public static void main(String[] args) throws IOException {
+        BufferedReader fin;
+        try{
+            fin = new BufferedReader(new FileReader("pancakes.in"));
+        }catch (IOException e) {
+            System.out.println("Could not open input file");
+        }
+    }
 }
